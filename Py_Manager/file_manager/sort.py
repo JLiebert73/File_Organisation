93b1c2e600d941b0bfa74d6d4c6895efb2ef3d8d
Vedic Dutta <109514492+JLiebert73@users.noreply.github.com>
2024-06-30 13:53:58 +0530

    added concurrent.futures

--- Py_Manager/file_manager/sort.py
+++ Py_Manager/file_manager/sort.py
@@ -1,6 +1,25 @@
 import os
+import concurrent.futures
 from .utils import create_directory, shift
 from .metadata import extract_image_metadata, extract_pdf_metadata, extract_docx_metadata, extract_xlsx_metadata, extract_pptx_metadata
+
+
+def move_file(source_path, destination_dir, categories):
+    filename = os.path.basename(source_path)
+    file_extension = os.path.splitext(filename)[1].lower()
+    if file_extension in categories:
+        destination_category = categories[file_extension]
+        destination_category_dir = os.path.join(destination_dir, destination_category)
+        create_directory(destination_category_dir)
+        destination_path = os.path.join(destination_category_dir, filename)
+        shift(source_path, destination_path)
+        return destination_category
+    else:
+        other_dir = os.path.join(destination_dir, 'Others')
+        create_directory(other_dir)
+        destination_path = os.path.join(other_dir, filename)
+        shift(source_path, destination_path)
+        return 'Others'
 
 
 def move_files(source_dir, destination_dir):
@@ -27,56 +46,42 @@ def move_files(source_dir, destination_dir):
         '.c': 'Code'
     }
 
-    folder_count = 0
-    image_count = 0
-    text_count = 0
-    video_count = 0
-    code_count = 0
-    other_count = 0
+    counts = {
+        'Folders': 0,
+        'Images': 0,
+        'Text': 0,
+        'Videos': 0,
+        'Code': 0,
+        'Others': 0
+    }
 
-    for filename in os.listdir(source_dir):
-        if filename == "Py_Manager" and os.path.isdir(os.path.join(source_dir, filename)):
-            continue
-        source_path = os.path.join(source_dir, filename)
-        if os.path.isfile(source_path):
-            file_extension = os.path.splitext(filename)[1].lower()
-            if file_extension in categories:
-                destination_category = categories[file_extension]
-                destination_category_dir = os.path.join(destination_dir, destination_category)
-                create_directory(destination_category_dir)
-                destination_path = os.path.join(destination_category_dir, filename)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for filename in os.listdir(source_dir):
+            if filename == "Py_Manager" and os.path.isdir(os.path.join(source_dir, filename)):
+                continue
+            source_path = os.path.join(source_dir, filename)
+            if os.path.isfile(source_path):
+                futures.append(executor.submit(move_file, source_path, destination_dir, categories))
+            elif os.path.isdir(source_path):
+                folders_dir = os.path.join(destination_dir, 'Folders')
+                create_directory(folders_dir)
+                destination_path = os.path.join(folders_dir, filename)
                 shift(source_path, destination_path)
-                if destination_category == 'Folders':
-                    folder_count += 1
-                elif destination_category == 'Images':
-                    image_count += 1
-                elif destination_category == 'Text':
-                    text_count += 1
-                elif destination_category == 'Videos':
-                    video_count += 1
-                elif destination_category == 'Code':
-                    code_count += 1
-            else:
-                other_dir = os.path.join(destination_dir, 'Others')
-                create_directory(other_dir)
-                destination_path = os.path.join(other_dir, filename)
-                shift(source_path, destination_path)
-                other_count += 1
-        elif os.path.isdir(source_path):
-            folders_dir = os.path.join(destination_dir, 'Folders')
-            create_directory(folders_dir)
-            destination_path = os.path.join(folders_dir, filename)
-            shift(source_path, destination_path)
-            folder_count += 1
+                counts['Folders'] += 1
+
+        for future in concurrent.futures.as_completed(futures):
+            category = future.result()
+            counts[category] += 1
 
     # Print the summary of files moved
     print("\n\nAll files sorted successfully!")
-    print("Folders moved:", folder_count)
-    print("Images moved:", image_count)
-    print("Text moved:", text_count)
-    print("Videos moved:", video_count)
-    print("Code moved:", code_count)
-    print("Others moved:", other_count)
+    print("Folders moved:", counts['Folders'])
+    print("Images moved:", counts['Images'])
+    print("Text moved:", counts['Text'])
+    print("Videos moved:", counts['Videos'])
+    print("Code moved:", counts['Code'])
+    print("Others moved:", counts['Others'])
 
 
 def main():
