f9d1ec7bedf434ce5c15310dfecbbb41c066496c
Vedic Dutta <109514492+JLiebert73@users.noreply.github.com>
2024-07-05 10:17:32 +0530

    Update utils.py

--- Py_Manager/file_manager/utils.py
+++ Py_Manager/file_manager/utils.py
@@ -1,27 +1,26 @@
 import os
-
+import shutil
+from tf_idf import HNSW
 
 def create_directory(directory_path):
     os.makedirs(directory_path, exist_ok=True)
 
-
 def shift(source_path, destination_path):
     try:
-        os.rename(source_path, destination_path)
-        print(f"Moved file: {source_path} to {destination_path}")
+        if os.path.isfile(source_path):
+            shutil.move(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            shutil.move(source_path, destination_path)
+        print(f"Moved: {source_path} to {destination_path}")
     except Exception as e:
-        print(f"Error moving file: {str(e)}")
-
+        print(f"Error moving {source_path} to {destination_path}: {e}")
 
 def main():
-    # Get the current working directory
+    hnsw = HNSW(m=16, ef_construction=200)
     path = os.getcwd()
     source_dir = path
     destination_dir = path
-
-    # Call the shift() function to move files
     shift(source_dir, destination_dir)
-
 
 if __name__ == "__main__":
     main()
